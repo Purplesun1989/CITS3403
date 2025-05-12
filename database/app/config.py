02facea930706381f6d513464655e4831e15c0fc
laylaVanStaden <layla.vanstaden@gmail.com>
2025-05-12 18:58:30 +0800

    Deleted unused file :)

--- database/app/config.py
+++
@@ -1,7 +0,0 @@
-import os 
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-default_database_path = 'sqlite:///'+os.path.join(basedir, 'app.db')
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or default_database_path
